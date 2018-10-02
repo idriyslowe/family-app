89a8f01cb19771676ece3ab4ceb9e9d34a02f150
idriyslowe <idriyswins@gmail.com>
2018-10-01 21:22:29 -0500

    Build basic Person class. Pass over controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,14 +1,13 @@
 class PeopleController < ApplicationController
 
   def create
-    # person = People.new(person_attributes)
-    # render json: person, status: :ok
-    render json: {data: 'hi'}, status: :ok
+    person = People.new(person_attributes)
+    render json: {data: person}, status: :ok
   end
 
   def index
-    # people = Person.all
-    render json: {data: 'hihi'}, status: :ok
+    people = Person.all
+    render json: {data: people}, status: :ok
   end
 
   def person_attributes
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,17 @@
 class Person
+  @@people = []
 
   def initialize options
+    @last_name = options[:last_name]
+    @first_name = options[:first_name]
+    @gender = options[:gender]
+    @favorite_color = options[:favorite_color]
+    @date_of_birth = options[:date_of_birth]
+
+    @@people << self
   end
 
+  def self.all
+    @@people
+  end
 end
